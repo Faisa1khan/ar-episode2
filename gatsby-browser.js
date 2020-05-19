22b9fb5dd6c5293a2482d6275d2ec04820f47383
faisa1khan <faisalkhan5464@gmail.com>
2020-05-19 16:18:39 +0530

    learning framer motion

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { GlobalProvider } from "./src/context/GlobalContext"
+
+export const wrapRootElement = ({ element }) => {
+  return <GlobalProvider>{element}</GlobalProvider>
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,38 @@ import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import { normalize } from "styled-normalize"
+import Header from "./Header"
+import {
+  useGlobalStateContext,
+  useGlobalDispatchContext,
+} from "../context/GlobalContext"
+import Cursor from "./CustomCursor"
+
+const GlobalStyle = createGlobalStyle`
+${normalize}
+* {
+  text-decoration: none;
+  cursor: none;
+}
+
+  html {
+    box-sizing: border-box;
+    --webkit-font-smoothing: antialiased;
+    font-size: 16px;
+
+  }
+
+  body {
+    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: ${props => props.theme.background};
+    overscroll-behavior: none;
+    overflow-x: hidden
+  }
+
+`
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -13,7 +45,34 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return <main>{children}</main>
+  const darkTheme = {
+    background: `#000`,
+    text: `#fff`,
+    red: `#ea291e`,
+  }
+  const lightTheme = {
+    background: `#fff`,
+    text: `#000`,
+    red: `#ea291e`,
+  }
+
+  const dispatch = useGlobalDispatchContext()
+
+  const { currentTheme, cursorStyles } = useGlobalStateContext()
+
+  const onCursor = cursorType => {
+    cursorType = (cursorStyles.includes(cursorType) && cursorType) || false
+    dispatch({ type: "CURSOR_THEME", cursorType: cursorType })
+  }
+
+  return (
+    <ThemeProvider theme={currentTheme === "dark" ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <Cursor />
+      <Header onCursor={onCursor} />
+      <main>{children}</main>
+    </ThemeProvider>
+  )
 }
 
 Layout.propTypes = {
